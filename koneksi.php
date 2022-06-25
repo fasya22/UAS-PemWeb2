3df39276fbdbbd561afdb8b051c073062750bede
Nur Halisah Fasya <69709745+fasya22@users.noreply.github.com>
2022-06-25 16:38:40 +0700

    Add files via upload

---
+++ koneksi.php
@@ -0,0 +1,14 @@
+<?php
+$conn_str = "host=john.db.elephantsql.com " .
+    "port=5432 " .
+    "user=iyfyhygy " .
+    "dbname=iyfyhygy " .
+    "password=ptBaSY0WkxrajtY6_7uv7Nk5eebYbAuu";
+$conn = pg_connect($conn_str);
+
+// if($conn) {
+//     echo "<h3>Koneksi Berhasil</h3>";
+// } else {
+//     echo "<h3>Koneksi GAGAL TERSAMBUNG</h3>";
+// }
+?>
